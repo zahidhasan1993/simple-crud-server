3375f28d2151d0646f21cbac6ddd0c2cab4a3564
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-10 18:06:55 +0600

    user update delete

--- index.js
+++ index.js
@@ -33,7 +33,30 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result)
     })
+    app.get("/users/:id", async (req,res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
 
+      res.send(user)
+    })
+    app.put("/users/:id",async (req,res) => {
+      const id = req.params.id;
+      const user = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedUser,options);
+
+      res.send(result)
+
+    })
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
